9eb8df7f661036feccc4fcbb574bdcfc910a7b2f
Peaky blinder <85233139+wasimfaris2@users.noreply.github.com>
2021-08-06 11:27:33 +0530

    Update purge.py

--- bot/modules/purge.py
+++ bot/modules/purge.py
@@ -1,8 +1,8 @@
 import time
 from telethon import events
 
-from SaitamaRobot import telethn
-from SaitamaRobot.modules.helper_funcs.telethn.chatstatus import (
+from bot import telethn
+from bot.modules.helper_funcs.telethn.chatstatus import (
     can_delete_messages,
     user_is_admin,
 )
